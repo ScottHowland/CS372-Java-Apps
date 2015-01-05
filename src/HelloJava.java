4423d97712f602115fb9d8a345f58ae5f8312319
Scott Howland <showland17@my.whitworth.edu>
2015-01-05 14:57:00 -0800

    Spanish println functionality achieved.

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World.\nAfternoon.");
+		System.out.println("Hola Mundo.\nAfternoon.");
 	}
 
 }
