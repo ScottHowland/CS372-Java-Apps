7de358567e814f98833d720063a9fe1cd710e226
WaveForceTyphoon <showland17@my.whitworth.edu>
2015-01-05 17:08:57 -0800

    HW1: PR1_1 and PR1_2

---
+++ PR1_12.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr1_1.pkg2;
+
+/**
+ *
+ * @author Scott
+ */
+public class PR1_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DataOnly d = new DataOnly();
+        
+        d.i = 47;
+        d.f = 1.1f;
+        d.b = false;
+        
+        System.out.printf("Integer: %d, Float: %f, Boolean: %s\n", d.i, d.f, d.b);        
+    }    
+}
