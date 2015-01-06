024d2be6520fcda252d1f7c310081fed25dce0da
WaveForceTyphoon <showland17@my.whitworth.edu>
2015-01-05 17:50:08 -0800

    Correct titling of PR1_1

---
+++ PR1_1/src/pr1_1/DataOnly.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr1_1;
+
+/**
+ *
+ * @author Scott
+ */
+public class DataOnly {
+    int i;
+    float f;
+    boolean b;
+}
---
+++ PR1_1/src/pr1_1/PR1_1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pr1_1;
+
+/**
+ *
+ * @author Scott
+ */
+public class PR1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DataOnly d = new DataOnly();
+        
+        d.i = 47;
+        d.f = 1.1f;
+        d.b = false;
+        
+        System.out.printf("Integer: %d, Float: %2f, Boolean: %s.\n", d.i, d.f, d.b);
+    }
+    
+}
